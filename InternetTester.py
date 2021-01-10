f654cd356eb63b39cbb9903b1a62a8db819679d8
Luka98122 <luka98122@gmail.com>
2021-01-10 13:16:05 +0100

    Detects errors and high ping. Padded code.

--- InternetTester.py
+++ InternetTester.py
@@ -1,35 +1,59 @@
 import sys
 import subprocess
-result = subprocess.run(
-    ["ping", "8.8.8.8"], capture_output=True, text=True)
 
-t = result.stdout.split(' ')
+result = subprocess.run(["ping", "8.8.8.8"], capture_output=True, text=True)
 
-def parseLine(inputLine): 
-    inputLine = inputLine.split('=')
-    inputLine = inputLine[2]
-    inputLine = inputLine.split('ms')
-    inputLine = inputLine[0]
-    return int(inputLine)
+t = result.stdout.split(" ")
 
-def pingServer (serverAddres):
+
+def parseLine(inputLine):
+
+    if inputLine == "Request timed out.":
+        return "Error"
+    else:
+        inputLine = inputLine.split("=")
+        inputLine = inputLine[2]
+        inputLine = inputLine.split("ms")
+        inputLine = inputLine[0]
+        return int(inputLine)
+
+
+def pingServer(serverAddres):
     Max = 0
     sumOfPings = 0
-    result = subprocess.run(
-    ["ping", serverAddres], capture_output=True, text=True)
-
-    t = result.stdout.split('\n')
-
+    result = subprocess.run(["ping", serverAddres], capture_output=True, text=True)
+    timeoutsAndErrors = 0
+    t = result.stdout.split("\n")
+    attentionMark = ""
     for i in range(4):
-        ping = parseLine(t[2+i])
-        sumOfPings = sumOfPings + ping
-        if ping > Max:
-            Max = ping
+        if parseLine(t[2 + i]) == "Error":
+            timeoutsAndErrors = timeoutsAndErrors + 1
+        else:
+            ping = parseLine(t[2 + i])
+            sumOfPings = sumOfPings + ping
+            if ping > Max:
+                Max = ping
 
-    Average = sumOfPings/4
-    return[Average, Max]
+    Average = sumOfPings / 4
+    if timeoutsAndErrors > 0:
+        attentionMark = "<---------"
+    return [Average, Max, timeoutsAndErrors, attentionMark]
 
-listOfServers = [["Google DNS", "8.8.8.8"], ["Main switch","192.168.86.63" ], ["Google Wifi Router", "192.168.86.1"], ["Cable modem", "192.168.100.1"], ["SBB DNS", "89.216.1.2"]]
+
+listOfServers = [
+    ["Google Wifi Router", "192.168.86.1", 30],
+    ["Main switch", "192.168.86.63", 40],
+    ["Cable modem", "192.168.100.1", 50],
+    ["SBB DNS", "89.216.1.2", 60],
+    ["Test server", "200.200.200.200", 10],
+    ["Google DNS", "8.8.8.8", 100],
+]
 for i in range(len(listOfServers)):
     info = pingServer(listOfServers[i][1])
-    print(str(listOfServers[i][0]) + "   IP: " + str(listOfServers[i][1]) + "   Average:" + str(info[0]) + "   Max:" + str(info[1]))
+
+    if info[1] > listOfServers[i][2]:
+        info[3] = "<--------- (Ping too high)"
+
+    print(
+        f"Name: {listOfServers[i][0]:20}  IP: {listOfServers[i][1]:16}  Average: {info[0]:5.1f}   Max: {info[1]:5}  Errors:  {info[2]:5}  {info[3]:15}"
+    )
